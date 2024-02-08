719f2d79ecc6515976b2619204084556a56f83be
Keyi Yin <keyi_yin@outlook.com>
2024-02-09 00:22:28 +0800

    sync upstream bucket template

--- bin/checkurls.ps1
+++ bin/checkurls.ps1
@@ -1,4 +1,4 @@
 if (!$env:SCOOP_HOME) { $env:SCOOP_HOME = Convert-Path (scoop prefix scoop) }
-$checkver = "$env:SCOOP_HOME/bin/checkver.ps1"
+$checkurls = "$env:SCOOP_HOME/bin/checkurls.ps1"
 $dir = "$PSScriptRoot/../bucket" # checks the parent dir
-& $checkver -Dir $dir @Args
+& $checkurls -Dir $dir @Args
